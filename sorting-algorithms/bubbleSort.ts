4dedbd10e96a80622643453cdf70c0aecf7b739c
lordcodex dev <adenirandaniel565@gmail.com>
2025-03-30 16:05:22 +0100

    implement bubble-sorting

--- sorting-algorithms/bubbleSort.ts
+++ sorting-algorithms/bubbleSort.ts
@@ -1,1 +1,22 @@
- 
+ export function bubbleSort(array: number[]) {
+    for(let n = array.length; n >=0;  n--){
+      for(let i = 0;  i < n - 1; i++){
+        if(array[i] > array[i + 1]) {
+            //swap elements
+            ;[array[i], array[i + 1]] = [array[i + 1], array[i]]
+        }
+      }  
+    }
+    return array
+ }
+
+ //before bubbleSorting
+ const arr = [2, 1, 5, 3, 4]
+
+ console.log(bubbleSort(arr))
+
+ //length is 5
+
+ //after bubbleSorting
+
+ const bubbArr = [1, 2, 3, 4, 5]
