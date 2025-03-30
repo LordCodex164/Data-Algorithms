7398ae9789e16643f31d24a1259b78812645dbad
LordCodex <adenirandaniel565@gmail.com>
2025-03-30 16:58:56 +0100

    implemented bubbleSort sorting method

--- sorting-algorithms/bubbleSort.ts
+++ sorting-algorithms/bubbleSort.ts
@@ -1,1 +1,14 @@
- 
+const bubbleSort = (arr:number[]) => {
+    for(let n = arr.length; n >= 0; n--){
+        for(let i = 0; i < n - 1; i++){
+            if(arr[i] > arr[i + 1]){
+                ;[arr[i], arr[i + 1]] = [arr[i + 1], arr[i]]
+            }
+        }
+    }
+    return arr
+}
+
+const arr = [1, 2, 10, 6]
+
+console.log(bubbleSort(arr))
