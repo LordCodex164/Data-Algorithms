9c8857ad2b959f134ed595a8510a48febb4d265b
lordcodex dev <adenirandaniel565@gmail.com>
2025-07-06 23:56:00 +0100

    big o notation

--- data-algorithms/add1.ts
+++ data-algorithms/add1.ts
@@ -7,7 +7,16 @@
  }
 
  var time1 = performance.now()
- console.log(addUpTo(9000000000))
+ console.log("add1", addUpTo(9000000000))
  var time2 = performance.now()
- console.log(2, time2)
  console.log("time elapsed", `${(time2 - time1)/1000}`, "seconds")
+
+ function addUpToN (n: number) {
+    let total = n * (n + 1)/2
+    return total
+ }
+
+ var time1_ = performance.now()
+ console.log("add2", addUpToN(9000000000))
+ var time2_ = performance.now()
+ console.log("time elapsed", `${(time2_ - time1_)/1000}`, "seconds")
